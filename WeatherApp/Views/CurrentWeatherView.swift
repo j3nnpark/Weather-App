614398372db4e428a1735bd87a8202dbc0e5b8d9
Jenn <j3nnkkin@gmail.com>
2023-02-28 12:56:36 +0900

    First commit

---
+++ WeatherApp/Views/CurrentWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentWeatherView.swift
+//  WeatherApp
+//
+//  Created by Jenn Park on 2023-02-28.
+//
+
+import SwiftUI
+
+struct CurrentWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurrentWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentWeatherView()
+    }
+}
