31bffed87adca51d4f7ca3487b964c475464c3e7
Jenn <j3nnkkin@gmail.com>
2023-02-28 12:44:11 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jenn Park on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
